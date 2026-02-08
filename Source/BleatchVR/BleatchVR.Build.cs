4771628d3f4246bfab4b2bbb0d6e2c51f4f047dd
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2026-02-08 11:37:22 +0100

    Squashed commit of the following:
    commit 69712a906bc7516614a852bd868c875695b63ee5
    Author: GamesLuk <lukas@meyer-zu-berstenhorst.de>
    Date:   Sun Feb 8 11:25:37 2026 +0100
    
        Change template to VRProject with keyboard support for development (#40)
    
        * Delete Source directory
    
        * Delete Config directory
    
        * Delete Content directory
    
        * Add files via upload
    
        * Create Empty file
    
        * Add Content
    
        * Add files via upload
    
        * Add files via upload
    
        * Add files via upload
    
        * Create Empty
    
        * Delete Content/Character/Empty
    
        * Create Empty
    
        * Add Mannequin character assets
    
        Import a comprehensive set of mannequin uasset files: animations (death, pistol, rifle, unarmed — aim, jog, walk, jump, hit reacts, attacks, montages), materials, meshes, rigs, and textures. These additions provide base character skeletons, animation states and visual assets for mannequin characters and weapon handling in the project.
    
        * Remove empty placeholder files
    
        Delete two empty files (Content/Characters/Empty and Content/Empty) to clean up the repository. These files contained no content and appear to be redundant placeholders.
    
        * Add BleatchVR Unreal module boilerplate
    
        Add initial Unreal Engine module and target files for the BleatchVR project. This includes game and editor Target classes (BleatchVRTarget, BleatchVREditorTarget), module build file (BleatchVR.Build.cs) with Core/CoreUObject/Engine/InputCore dependencies, the module header (BleatchVR.h), and the module implementation (BleatchVR.cpp) registering the primary game module. DefaultBuildSettings set to V6.
    
    commit d6e0276c5b85d590daa1e2c0978a50c81a6412d2
    Author: GamesLuk <lukas@meyer-zu-berstenhorst.de>
    Date:   Sun Feb 8 11:02:53 2026 +0100
    
        Add new Config files

--- Source/BleatchVR/BleatchVR.Build.cs
+++ Source/BleatchVR/BleatchVR.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 
@@ -7,38 +7,14 @@ public class BleatchVR : ModuleRules
 	public BleatchVR(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"InputCore",
-			"GeometryCollectionEngine",
-			"ChaosSolverEngine",
-			"FieldSystemEngine",
-			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
-			"UMG",
-			"Slate"
-		});
-
-		PrivateDependencyModuleNames.AddRange(new string[] { });
-
-		PublicIncludePaths.AddRange(new string[] {
-			"BleatchVR",
-			"BleatchVR/Variant_Horror",
-			"BleatchVR/Variant_Horror/UI",
-			"BleatchVR/Variant_Shooter",
-			"BleatchVR/Variant_Shooter/AI",
-			"BleatchVR/Variant_Shooter/UI",
-			"BleatchVR/Variant_Shooter/Weapons"
-		});
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
+		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
