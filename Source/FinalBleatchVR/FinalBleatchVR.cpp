c76d87ab523aee2d814002b195f4de90ee20cf9f
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2026-02-09 21:59:39 +0100

    Recreate project in first person with vr content, world, ...

---
+++ Source/FinalBleatchVR/FinalBleatchVR.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FinalBleatchVR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FinalBleatchVR, "FinalBleatchVR" );
+
+DEFINE_LOG_CATEGORY(LogFinalBleatchVR)
